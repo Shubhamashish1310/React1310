47759023e852c33c92079be38c2c84346bd0a28a
Shubhamashish1310 <98949989+Shubhamashish1310@users.noreply.github.com>
2024-07-24 15:26:19 +0530

    add

--- Basic React 01/src/App.jsx
+++ Basic React 01/src/App.jsx
@@ -1,12 +1,12 @@
 
 import Sort from "./shubham1.jsx"
 
-function App(c) {
+function App() {
   return (  
     <div style={{textAlign:"center",backgroundColor:"cyan",display:"flex",justifyContent:"center",
     flexDirection:"column",alignItems:"center",height:"100vh"}}>
 
-      <h1>My First Project On React</h1>
+      <h1>!My First Project On React!</h1>
      <Sort></Sort>
       
     </div>
