f55dea839f91e87a7fcb7008b384c4e6f3c827d8
Shubhamashish1310 <98949989+Shubhamashish1310@users.noreply.github.com>
2025-06-07 00:15:04 +0530

    practicing

---
+++ react/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-red-400'>
+        <h1>hello shubham</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
